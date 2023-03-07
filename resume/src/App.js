b272cbf2da47935e51649081a2bb01a155763b10
Kelsey Kuno <kelsey.kuno@gmail.com>
2023-03-07 16:08:46 -0600

    old styles imported into new repo

--- resume/src/App.js
+++ resume/src/App.js
@@ -4,21 +4,27 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* <img src={logo} className="App-logo" alt="logo" /> */}
+      <div className="content-container">
+        <button className="content">About</button>
+        <div className="content">Resume</div>
+        <div className="content">Projects</div>
+      </div>
+      <div className="footer">
+        <div className="logo">
+          Kelsey <br /> Kuno
+        </div>
+        <div className="title-contact-container">
+          <div className="title">software engineer & designer</div>
+          <div className="contact-container">
+            <div className="contact"><span className="number">01</span>LinkedIn</div>
+            <div className="contact"><span className="number">02</span>GitHub</div>
+            <div className="contact"><span className="number">03</span>Creative Portfolio</div>
+          </div>
+        </div>
+      </div>
     </div>
+
   );
 }
 
