37a93a393a94abb7a59fda01a3d48bbfe97dccb0
Kelsey Kuno <kelsey.kuno@gmail.com>
2023-04-11 20:40:20 -0500

    home page links added

--- resume/src/components/Nav/Nav.jsx
+++ resume/src/components/Nav/Nav.jsx
@@ -1,32 +1,41 @@
 import React from 'react';
-// import {useHistory} from 'react-router-dom';
-
-// const history = useHistory();
-
-// const routeHome = () => {
-//     alert("You are headed to animals");
-//     ///CHANGE LOCATION???
-//     history.push('/resume');
-//   }; 
 
 
 function Nav() {
-    return(
+    return (
         <div className="footer">
-          <div
-          onClick={() => console.log('hi')}
-          className="logo"
-          >
-            Kelsey <br /> Kuno
-          </div>
-          <div className="title-contact-container">
-            <div className="title">software engineer & designer</div>
-            <div className="contact-container">
-              <div className="contact"><span className="number">01</span>LinkedIn</div>
-              <div className="contact"><span className="number">02</span>GitHub</div>
-              <div className="contact"><span className="number">03</span>Creative Portfolio</div>
+            <div
+                onClick={() => console.log('hi')}
+                className="logo"
+            >
+                Kelsey <br /> Kuno
             </div>
-          </div>
+            <div className="title-contact-container">
+                <div className="title">software engineer & designer</div>
+                <div className="contact-container">
+                    <div className="contact">
+                        <span className="number">01</span>
+                        <a href="https://www.linkedin.com/in/kelsey-kuno/"
+                            target="blank">
+                            LinkedIn
+                        </a>
+                    </div>
+                    <div className="contact">
+                        <span className="number">02</span>
+                        <a href="https://github.com/kelkuno"
+                            target="blank">
+                            GitHub
+                        </a>
+                    </div>
+                    <div className="contact">
+                        <span className="number">03</span>
+                        <a href="https://www.kelseykuno.com/"
+                            target="blank">
+                            Creative Portfolio
+                        </a>
+                    </div>
+                </div>
+            </div>
         </div>
 
     );
