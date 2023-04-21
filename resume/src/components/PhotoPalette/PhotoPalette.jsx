13f432b554a033a11d8e38b66eed51fe5cbaa9c1
Kelsey Kuno <kelsey.kuno@gmail.com>
2023-04-20 19:40:48 -0500

    added photo palette content

--- resume/src/components/PhotoPalette/PhotoPalette.jsx
+++ resume/src/components/PhotoPalette/PhotoPalette.jsx
@@ -1,9 +1,18 @@
 import React from 'react';
 
 
-function PhotoPalette (){
-    return(
-        <h1>hiiiiii i'm in photo palette</h1>
+function PhotoPalette() {
+    return (
+        <>
+            <h2>Photo Palette</h2>
+            <h3>Project Duration: 2 weeks!</h3>
+            <h3>Application Overview:</h3>
+            <p>Color is a critical tool for artists and designers. We are surrounded by interesting color combinations all of the time. I might notice a cool color combination in a pattern on someone's shirt or displayed on a package logo or in a piece of artwork. I might go for a walk and see a striking scene in nature with a great color palette. Color inspirations are everywhere but if we don’t have a way to record and save them, then we won't remember them. </p>
+            <p>My app will generate color palettes from photos that users upload. The idea is a user will upload a photo and the app will generate colors in the photo so that the user can store and access the palette later on. </p>
+            <p>Being that I am an artist myself, I spent a lot of time thinking about the application flow. Here's a rough wireframe of how I imagined the flow:</p>
+
+        </>
+
 
     );
 
