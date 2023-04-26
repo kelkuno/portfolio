67c5a638fecb0cd69d5f05b08aaf2d44c3983096
Kelsey Kuno <kelsey.kuno@gmail.com>
2023-04-25 20:53:13 -0500

    secondary nav restructured for responsiveness

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -7,39 +7,44 @@ function Nav() {
             <div className="footer">
                 <div className="logo">
                     <h1
-                    onClick={() => console.log('hi')}
+                        onClick={() => console.log('hi')}
                     >
                         Kelsey <br /> Kuno
                     </h1>
                     <div className="nav-text">software engineer & designer</div>
-
-
                 </div>
-                <div className="title-contact-container">
-                    {/* <div className="contact-container">
-                        <div className="contact">
-                            <span className="number">01</span>
-                            <a href="https://www.linkedin.com/in/kelsey-kuno/"
-                                target="blank">
-                                LinkedIn
-                            </a>
-                        </div>
-                        <div className="contact">
-                            <span className="number">02</span>
-                            <a href="https://github.com/kelkuno"
-                                target="blank">
-                                GitHub
-                            </a>
-                        </div>
-                        <div className="contact">
-                            <span className="number">03</span>
-                            <a href="https://www.kelseykuno.com/"
-                                target="blank">
-                                Creative Portfolio
-                            </a>
-                        </div>
-                    </div> */}
+
+                <div className="contact-container">
+                <div className="contact">
+                        <span className="number">01</span>
+                        <a href="https://kelkuno.github.io/portfolio/#/"
+                            >
+                            Home
+                        </a>
+                    </div>
+                    <div className="contact">
+                        <span className="number">01</span>
+                        <a href="https://www.linkedin.com/in/kelsey-kuno/"
+                            target="blank">
+                            LinkedIn
+                        </a>
+                    </div>
+                    <div className="contact">
+                        <span className="number">02</span>
+                        <a href="https://github.com/kelkuno"
+                            target="blank">
+                            GitHub
+                        </a>
+                    </div>
+                    <div className="contact">
+                        <span className="number">03</span>
+                        <a href="https://www.kelseykuno.com/"
+                            target="blank">
+                            Creative Portfolio
+                        </a>
+                    </div>
                 </div>
+
             </div>
         </>
 
